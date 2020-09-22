54767c94887f5bed99eecdf57856d3bc1c016940
KeremTurkyilmaz <kerem.turkyilmaz@outlook.it>
2020-09-22 11:50:18 +0200

    add reset css

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: ['@/assets/styles/main.scss'],
+  css: ['@/assets/styles/partials/reset.css', '@/assets/styles/main.scss'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
