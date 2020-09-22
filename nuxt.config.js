c32b43c3682d197cab7929e3b543e8402de35217
KeremTurkyilmaz <kerem.turkyilmaz@outlook.it>
2020-09-22 11:16:31 +0200

    add basic css config and global scss variables

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
+  css: ['@/assets/styles/main.scss'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
@@ -32,7 +32,11 @@ export default {
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [],
+  modules: ['@nuxtjs/style-resources'],
+
+  styleResources: {
+    scss: ['@/assets/styles/variables.scss'],
+  },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,9 @@
 <template>
-  <div class="container">
-  </div>
+  <div class="container"></div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style>
-</style>
+<style></style>
