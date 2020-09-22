2a8dc9f6f6a05cac2507ca60ea59145285525ee6
KeremTurkyilmaz <kerem.turkyilmaz@outlook.it>
2020-09-22 12:16:03 +0200

    add vue-svg-inline-loader

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,5 +39,15 @@ export default {
   },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {},
+  build: {
+    extend(config, { isServer, isClient }) {
+      config.module.rules.push({
+        test: /\.vue$/,
+        loader: 'vue-svg-inline-loader',
+        options: {
+          /* ... */
+        },
+      })
+    },
+  },
 }
