655b21980a9d02d03b9f71525452a98df626acbc
KeremTurkyilmaz <kerem.turkyilmaz@outlook.it>
2020-09-22 15:36:45 +0200

    add basic about page

---
+++ components/About.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="sec about">
+    <h1>
+      If you want to get to know us, find out if we are available for a project, submit your
+      portfolio or interact with us in any other way, here is where you can find us.
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.about {
+  background-color: $col-violet;
+  h1 {
+    width: 70%;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
     <full-page ref="fullpage" :options="options">
+      <About class="section" />
       <div class="section">Section 1</div>
       <div class="section">Section 2</div>
     </full-page>
@@ -8,13 +9,21 @@
 </template>
 
 <script>
+import About from '~/components/About'
 export default {
+  components: {
+    About,
+  },
   data() {
     return {
-      options: {},
+      // Vue-FullPage Options
+      options: {
+        licenseKey: 'YOUR_KEY_HEERE',
+        scrollingSpeed: 600,
+      },
     }
   },
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped></style>
