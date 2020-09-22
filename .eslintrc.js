5fc2b4ad59cd9e36a721c3f929f7ecfea9600c0d
KeremTurkyilmaz <kerem.turkyilmaz@outlook.it>
2020-09-22 16:56:50 +0200

    fix linter and vscode settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,24 +2,22 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true,
+    node: true
   },
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: 'babel-eslint'
   },
-  extends: [
-    '@nuxtjs',
-    'prettier',
-    'prettier/vue',
-    'plugin:prettier/recommended',
-    'plugin:nuxt/recommended',
-  ],
-  plugins: ['prettier'],
+  extends: ['@nuxtjs', 'prettier', 'plugin:nuxt/recommended'],
+  plugins: ['vue'],
   // add your custom rules here
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     quotes: ['warn', 'single', { allowTemplateLiterals: true }],
     'linebreak-style': 'off',
-  },
+    camelcase: [0, { properties: 'never' }],
+    'vue/multiline-html-element-content-newline': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/html-self-closing': 'off',
+  }
 }
