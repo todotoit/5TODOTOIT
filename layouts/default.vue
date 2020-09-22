acd3c6b86bba0eaaa375e579864532e2a456a967
KeremTurkyilmaz <kerem.turkyilmaz@outlook.it>
2020-09-22 17:55:43 +0200

    delete logo from layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,9 @@
 <template>
   <div>
-    <img svg-inline class="logo" src="@/assets/icons/TODO_LOGO.svg" />
     <Nuxt />
   </div>
 </template>
 
-<style lang="scss" scoped>
-.logo {
-  position: fixed;
-  top: $padding;
-  left: $padding;
-  z-index: 100;
-  path {
-    fill: $col-white;
-  }
-  &:focus {
-    outline: none;
-  }
-}
-</style>
+<script></script>
+
+<style lang="scss" scoped></style>
