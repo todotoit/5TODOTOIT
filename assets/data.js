44c058f6684059218c69e38e083dfea4165d5cb7
KeremTurkyilmaz <kerem.turkyilmaz@outlook.it>
2020-09-23 14:25:23 +0200

    add copies and team data

---
+++ assets/data.js
@@ -0,0 +1,99 @@
+export const substatements = [
+    {
+      label: 'sub01',
+      copy:
+        'We tailor experiences that shape and improve the relationship between people and brands. Try and keep up with us.',
+      file: '~/assets/video/sub01.mp4'
+    },
+    {
+      label: 'sub02',
+      copy:
+        'We make data meaningful, engaging, easy to grasp. Did we just turn statistics into fun for you? You’re welcome.',
+      file: '~/assets/video/sub02.mp4'
+    },
+    {
+      label: 'sub03',
+      copy:
+        'We help our clients innovate with intuition, inspiration, technical mastery and beauty. Always finding new ways to stand out.',
+      file: '~/assets/video/sub03.mp4'
+    },
+    {
+      label: 'sub04',
+      copy:
+        'We blend physical and digital, technology and the human touch. It’s the next normal, and we’ve been doing it since 2007.',
+      file: '~/assets/video/sub04.mp4'
+    }
+  ]
+  
+  export const people = [
+    {
+      name: 'Andrea Clemente',
+      jobs: 'Co-Founder & Managing director',
+      file: '~/assets/people/andreaclemente.gif'
+    },
+    {
+      name: 'Fabio Franchino',
+      jobs: 'Co-Founder & CTA',
+      file: '~/assets/people/fabiofranchino.gif'
+    },
+    {
+      name: 'Michelle Nebiolo',
+      jobs: 'Content & Project Manager',
+      file: '~/assets/people/michellenebiolo.gif'
+    },
+    {
+      name: 'Julie Faure',
+      jobs: 'Project Manager',
+      file: '~/assets/people/juliefaure.gif'
+    },
+    {
+      name: 'Marco Aimo',
+      jobs: 'Full Stack Developer',
+      file: '~/assets/people/marcoaimo.gif'
+    },
+    {
+      name: 'Luca Zanconi',
+      jobs: 'Native & 3D Software developer',
+      file: '~/assets/people/lucazanconi.gif'
+    },
+    {
+      name: 'Emilio Bondioli',
+      jobs: 'Interaction designer & Developer',
+      file: '~/assets/people/emiliobondioli.gif'
+    },
+    {
+      name: 'Kerem Türkyilmaz',
+      jobs: 'Designer & Junior Developer',
+      file: '~/assets/people/keremturkyilmaz.gif'
+    },
+    {
+      name: 'Marta Monge',
+      jobs: 'Art Director e Visual designer',
+      file: '~/assets/people/martamonge.gif'
+    },
+    {
+      name: 'Federica Zucchini',
+      jobs: 'Interaction Designer',
+      file: '~/assets/people/federicazucchini.gif'
+    },
+    {
+      name: 'Matteo Palu',
+      jobs: 'Graphic Designer',
+      file: '~/assets/people/matteopalu.gif'
+    },
+    {
+      name: 'Virginia Toffetti',
+      jobs: 'Product Designer',
+      file: '~/assets/people/virginiatoffetti.gif'
+    },
+    {
+      name: 'Edoardo Falletta',
+      jobs: 'Business Development',
+      file: '~/assets/people/edoardofalletta.gif'
+    },
+    {
+      name: 'Alessandra Martina',
+      jobs: 'Administrative Assistant',
+      file: '~/assets/people/alessandramartina.gif'
+    }
+  ]
