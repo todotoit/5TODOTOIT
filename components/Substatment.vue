8d8d63dee5d7833ffb94f7517c0020029df3a54b
KeremTurkyilmaz <kerem.turkyilmaz@outlook.it>
2020-09-24 10:21:13 +0200

    create substatment page

---
+++ components/Substatment.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="substatment" :class="{ 'has-video': currentVideo }">
+    <div class="top">
+      <h1 class="title">
+        {{ copy }}
+      </h1>
+    </div>
+    <div class="bottom">
+      <Grid :actions="actions" />
+    </div>
+    <div v-show="currentVideo" class="background-video">
+      <video
+        loop
+        autoplay
+        muted
+        playsinline
+        class="video"
+        :src="currentVideo"
+      ></video>
+    </div>
+  </div>
+</template>
+
+<script>
+import Grid from '~/components/Grid/Grid'
+export default {
+  name: 'Substatment',
+  components: {
+    Grid
+  },
+  data() {
+    return {
+      defaultSubstatment:
+        'Experiences are more than the sum of their parts. And what we do is more than the projects in our portfolio. Always be exploring.'
+    }
+  },
+  computed: {
+    substatment() {
+      return this.$store.getters['grid/substatement']
+    },
+    copy() {
+      return this.substatment ? this.substatment.copy : this.defaultSubstatment
+    },
+    currentVideo() {
+      return this.substatment ? this.substatment.file : null
+    },
+    actions() {
+      return this.$store.getters['grid/actions']('substatements')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.substatment {
+  background-color: $col-violet;
+  &.has-video {
+    background-color: transparent;
+  }
+  .background-video {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    overflow-y: hidden;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    z-index: -1;
+    .video {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,22 @@
 <template>
   <div class="container">
-    <img svg-inline class="logo" src="@/assets/icons/TODO_LOGO.svg" @click="backToTheTop" />
-    <full-page ref="fullpage" :options="options">
-      <Home class="section" />
-      <About class="section" />
-    </full-page>
+    <img svg-inline class="logo" src="@/assets/icons/TODO_LOGO.svg" />
+    <Home class="section" />
+    <Substatment class="section" />
+    <About class="section" />
   </div>
 </template>
 
 <script>
 import Home from '~/components/Home'
+import Substatment from '~/components/Substatment'
 import About from '~/components/About'
+
 export default {
   components: {
     Home,
+    Substatment,
     About
-  },
-  data() {
-    return {
-      // Vue-FullPage Options
-      options: {
-        licenseKey: 'YOUR_KEY_HEERE',
-        anchors: ['home', 'substatment', 'team', 'about'],
-        scrollingSpeed: 600
-      }
-    }
-  },
-  mounted() {
-    this.fullpageApi = this.$refs.fullpage.api
-  },
-  methods: {
-    backToTheTop() {
-      this.fullpageApi.moveTo('home', 0)
-    }
   }
 }
 </script>
