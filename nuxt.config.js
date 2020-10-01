7535b6b414c16cefdbe1e8074926dfca053a3a20
Kerem Türkyilmaz <kerem.turkyilmaz@outlook.it>
2020-10-01 15:06:37 +0200

    add server configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,11 @@ export default {
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
   ssr: false,
 
+  server: {
+    port: 3000, // default: 3000
+    host: '0.0.0.0' // default: localhost
+  },
+  
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
 
