8ec594ea0a179c335f307f4df3398249907d7174
Kerem Türkyilmaz <kerem.turkyilmaz@outlook.it>
2020-10-01 11:21:43 +0200

    udpate data.js

--- assets/data.js
+++ assets/data.js
@@ -1,99 +1,129 @@
 export const substatements = [
   {
+    id: 1,
     label: 'sub01',
     copy:
       'We tailor experiences that shape and improve the relationship between people and brands. Try and keep up with us.',
-    file: '/videos/sub_01.webm'
+    file: {
+      horizontal: '/videos/main_horizontal.mp4',
+      vertical: '/videos/main_vertical.mp4'
+    }
   },
   {
+    id: 2,
     label: 'sub02',
     copy:
       'We make data meaningful, engaging, easy to grasp. Did we just turn statistics into fun for you? You’re welcome.',
-    file: '/videos/sub_02.webm'
+    file: {
+      horizontal: '/videos/main_horizontal.mp4',
+      vertical: '/videos/main_vertical.mp4'
+    }
   },
   {
+    id: 3,
     label: 'sub03',
     copy:
       'We help our clients innovate with intuition, inspiration, technical mastery and beauty. Always finding new ways to stand out.',
-    file: '/videos/sub_03.webm'
+    file: {
+      horizontal: '/videos/main_horizontal.mp4',
+      vertical: '/videos/main_vertical.mp4'
+    }
   },
   {
+    id: 4,
     label: 'sub04',
     copy:
       'We blend physical and digital, technology and the human touch. It’s the next normal, and we’ve been doing it since 2007.',
-    file: '/videos/sub_04.webm'
+    file: {
+      horizontal: '/videos/main_horizontal.mp4',
+      vertical: '/videos/main_vertical.mp4'
+    }
   }
 ]
 
 export const team = [
   {
+    id: 1,
     name: 'Andrea Clemente',
     jobs: 'Co-Founder & Managing director',
-    file: '/team/andreaclemente.gif'
+    file: '/team/michellenebiolo.gif'
   },
   {
+    id: 2,
     name: 'Fabio Franchino',
     jobs: 'Co-Founder & CTO',
-    file: '/team/fabiofranchino.gif'
+    file: '/team/michellenebiolo.gif'
   },
   {
+    id: 3,
     name: 'Michelle Nebiolo',
     jobs: 'Content & Project Manager',
     file: '/team/michellenebiolo.gif'
   },
   {
+    id: 4,
     name: 'Julie Faure',
     jobs: 'Project Manager',
-    file: '/team/juliefaure.gif'
+    file: '/team/michellenebiolo.gif'
   },
   {
+    id: 5,
     name: 'Marco Aimo',
     jobs: 'Full Stack Developer',
-    file: '/team/marcoaimo.gif'
+    file: '/team/michellenebiolo.gif'
   },
   {
+    id: 6,
     name: 'Luca Zanconi',
     jobs: 'Native & 3D Software developer',
-    file: '/team/lucazanconi.gif'
+    file: '/team/michellenebiolo.gif'
   },
   {
+    id: 7,
     name: 'Emilio Bondioli',
     jobs: 'Interaction designer & Developer',
-    file: '/team/emiliobondioli.gif'
+    file: '/team/michellenebiolo.gif'
   },
   {
+    id: 8,
     name: 'Kerem Türkyilmaz',
     jobs: 'Designer & Junior Developer',
-    file: '/team/keremturkyilmaz.gif'
+    file: '/team/michellenebiolo.gif'
   },
   {
+    id: 9,
     name: 'Marta Monge',
     jobs: 'Art Director e Visual designer',
-    file: '/team/martamonge.gif'
+    file: '/team/michellenebiolo.gif'
   },
   {
+    id: 10,
     name: 'Federica Zucchini',
     jobs: 'Interaction Designer',
-    file: '/team/federicazucchini.gif'
+    file: '/team/michellenebiolo.gif'
   },
   {
+    id: 11,
     name: 'Matteo Palu',
     jobs: 'Graphic Designer',
-    file: '/team/matteopalu.gif'
+    file: '/team/michellenebiolo.gif'
   },
   {
+    id: 12,
     name: 'Virginia Toffetti',
     jobs: 'Product Designer',
-    file: '/team/virginiatoffetti.gif'
+    file: '/team/michellenebiolo.gif'
   },
   {
+    id: 13,
     name: 'Edoardo Falletta',
     jobs: 'Business Development',
-    file: '/team/edoardofalletta.gif'
+    file: '/team/michellenebiolo.gif'
   },
   {
+    id: 14,
     name: 'Alessandra Martina',
     jobs: 'Administrative Assistant',
-    file: '/team/alessandramartina.gif'
+    file: '/team/michellenebiolo.gif'
   }
 ]
