7f41cf24f86a77e9475e334b1bdedae71abd8d77
Kerem Türkyilmaz <kerem.turkyilmaz@outlook.it>
2020-10-02 16:39:20 +0200

    add normalize-wheel

--- plugins/fullpage.js
+++ plugins/fullpage.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import { normalizeWheel } from '~/plugins/vendor/normalize-scroll'
 
 const currentScroll = 0
 const scrollOffset = 100
@@ -9,17 +10,20 @@ let offsetMobile = 0
 Vue.directive('fullpage-scroll', {
   inserted(el, binding) {
     const options = binding.value
-    console.log(options)
     let timeout
+    let lastspin = 0
     const handleScroll = function (event) {
+      const wheel = normalizeWheel(event)
       event.preventDefault()
       event.stopPropagation()
-      console.log(event.deltaY)
-      if (timeout) clearTimeout(timeout)
+      if(Math.abs(wheel.spinY) <= 0.025) lastspin = 0
+      if (timeout || Math.abs(lastspin) >= Math.abs(wheel.spinY)) return
+      lastspin = wheel.spinY
       timeout = setTimeout(() => {
-        if (event.deltaY > 10) options.callback(1)
-        if (event.deltaY < -10) options.callback(-1)
+        timeout = null
       }, options.delay)
+      if (wheel.spinY > 0.125) options.callback(1)
+      if (wheel.spinY < -0.125) options.callback(-1)
     }
     el.addEventListener('mousewheel', handleScroll)
     el.addEventListener('touchstart', (e) => {
