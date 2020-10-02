9fc278d2055de7adbe47de80a498029b8e40d739
Kerem Türkyilmaz <kerem.turkyilmaz@outlook.it>
2020-10-02 12:13:57 +0200

    sections store refactor

--- store/sections.js
+++ store/sections.js
@@ -1,24 +1,24 @@
 export const state = () => ({
+  current: 0,
   sections: [
     { id: 0, target: '#home', name: 'Home' },
     { id: 1, target: '#substatement', name: 'Substatement' },
     { id: 2, target: '#team,', name: 'Team' },
     { id: 3, target: '#about', name: 'About' }
   ],
-  section: { id: 0, target: '#home', name: 'Home' }
 })
 
 export const getters = {
   sections: (state) => {
     return state.sections
   },
-  section: (state) => {
-    return state.section
+  current: (state) => {
+    return state.current
   }
 }
 
 export const mutations = {
-  setSection(state, data) {
-    state.page = data
+  setCurrent(state, data) {
+    state.current = data
   }
 }
