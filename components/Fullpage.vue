b68a08498582d0c7f09e1327bda7677cc3e80a78
Emilio Bondioli <e.bondioli@todo.to.it>
2021-01-11 16:19:16 +0100

    use one-scroll for scroll normalization

--- components/Fullpage.vue
+++ components/Fullpage.vue
@@ -5,6 +5,7 @@
 </template>
 
 <script>
+import { One } from '@ebondioli/one-scroll'
 import { clamp } from '~/utils'
 const SCROLL_DELAY = 1200
 
@@ -20,39 +21,38 @@ export default {
   mounted() {
     const children = this.$children
     this.sections = children.map((c) => c.$el)
-    this.$el.addEventListener('wheel', this.scroll)
-    this.$el.addEventListener('touchstart', (e) => {
-      this.touchStart = e.touches[0].clientY
-    })
-    this.$el.addEventListener('touchmove', this.handleTouch, {
-      passive: false,
-      cancelable: true
-    })
+    this.one = new One(this.$el)
+    this.one.addEventListener('*', this.scroll)
   },
   destroyed() {
-    this.$el.removeEventListener('wheel', this.scroll)
+    // this.$el.removeEventListener('wheel', this.scroll)
   },
   methods: {
     scroll(e) {
-      const dir = Math.sign(e.deltaY)
+      const dir = e.one.direction
+      console.log(
+        e.one.id,
+        'inertial:',
+        e.one.inertial,
+        e.one.target,
+        this.hasScrollableInPath(e.one.target, dir)
+      )
       // allows scrolling inside the section without triggering global navigation
-      if (this.hasScrollableInPath(e.target, dir)) return false
-      this.move(dir)
-      // prevent default browser scrolling
+      if (this.hasScrollableInPath(e.one.target, dir)) {
+        console.log('scrollable', e.one.target)
+        return false
+      }
+      e.stopPropagation()
       e.preventDefault()
+      // prevent default browser scrolling
+      if (!e.one.inertial) this.move(dir)
       return false
-    },
-    handleTouch(e) {
-      // checks if minimum offset threshold for scrolling has been reached
-      const offsetMobile = this.touchStart - e.touches[0].clientY
-      e.deltaY = e.wheelDelta = Math.sign(offsetMobile) * 2
-      if (Math.abs(offsetMobile) > 8) return this.scroll(e)
     },
     move(direction) {
       const next = clamp(this.current + direction, 0, this.sections.length - 1)
-      // console.log(`move from: ${this.current}, to: ${this.current}`, this.lock ? 'LOCKED' : '')
 
       if (this.lock !== null) return
+      console.log(`move from: ${this.current}, to: ${next}`)
       // scroll to current section
       this.scrollTo(next)
       this.current = next
@@ -67,14 +67,19 @@ export default {
         // target is scrollable
         const scrollHeight = target.scrollHeight - target.clientHeight
         const scrollOffset = Math.abs(target.scrollTop - scrollHeight)
-        return (
+        const isScrollable =
           (scrollOffset > 10 && dir > 0) || // end of scroll area, going down
           (scrollOffset <= scrollHeight - 10 && dir < 0) // begin of scroll area, going up
-        )
+
+        if (isScrollable) return true
+        else if (target.parentElement !== this.$el)
+          return this.hasScrollableInPath(target.parentElement, dir)
       } else if (target.parentElement !== this.$el) {
         // target is not scrollable, check parent or bail if root $el
         return this.hasScrollableInPath(target.parentElement, dir)
-      } else return false // target is not scrollable
+      } else {
+        return false // target is not scrollable
+      }
     },
     scrollTo(idx) {
       this.$scrollTo(this.sections[idx], 700, {
