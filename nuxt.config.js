577ca6314e1e2d590ea82f39e793e5f4d13ddf4a
Kerem Türkyilmaz <kerem.turkyilmaz@outlook.it>
2021-02-10 11:30:19 +0100

    google analytics setup

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,8 +35,14 @@ export default {
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module'
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/google-analytics'
   ],
+
+  googleAnalytics: {
+    // Options
+    id: 'UA-7954920-1'
+  },
 
   // Configure polyfills:
   polyfill: {
