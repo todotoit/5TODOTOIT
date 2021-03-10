b493eb75fd8510332db5e8b987d295ee27f71c85
Emilio Bondioli <e.bondioli@todo.to.it>
2021-03-10 12:10:10 +0100

    initially hide substatement videos

--- components/Sections/Substatement.vue
+++ components/Sections/Substatement.vue
@@ -3,27 +3,41 @@
     <section class="substatment">
       <div class="top">
         <transition name="fade" mode="out-in">
-          <h1 :key="title" v-animate-in="{ delay: 0.1 }" class="title">{{ title }}</h1>
+          <h1 :key="title" v-animate-in="{ delay: 0.1 }" class="title">
+            {{ title }}
+          </h1>
         </transition>
         <div class="controls">
           <transition name="fade" mode="out-in">
-            <div v-if="!currentCopy" :key="'buttons'" v-animate-in="{ delay: 0.15 }" class="cta-link">
+            <div
+              v-if="!currentCopy"
+              :key="'buttons'"
+              v-animate-in="{ delay: 0.15 }"
+              class="cta-link"
+            >
               <p>CURIOUS? CLICK ON THE <span>RED DOTS</span></p>
             </div>
-            <div v-else :key="'button'" class="cta-link btn-active" @click.prevent="close"><p>CLOSE</p></div>
+            <div
+              v-else
+              :key="'button'"
+              class="cta-link btn-active"
+              @click.prevent="close"
+            >
+              <p>CLOSE</p>
+            </div>
           </transition>
         </div>
       </div>
-      <div v-show="currentCopy" class="bg-anim">
+      <div v-if="currentCopy" class="bg-anim">
         <video
           v-for="(v, i) in videoAssets"
-          v-show="currentCopy ? v.id === currentCopy.id : false"
           :key="i"
           loop
           autoplay
           muted
           playsinline
           class="video"
+          :class="{ visible: currentCopy ? v.id === currentCopy.id : false }"
           :poster="v.file.poster[ratio]"
           :src="v.file.videos[ratio]"
         ></video>
@@ -42,7 +56,8 @@ export default {
   },
   data() {
     return {
-      defaultCopy: 'Design and technology are not our product, but our creative tools. That’s why what we can do for you is more than the projects in our portfolio.'
+      defaultCopy:
+        'Design and technology are not our product, but our creative tools. That’s why what we can do for you is more than the projects in our portfolio.'
     }
   },
   computed: {
@@ -77,6 +92,10 @@ export default {
       width: 100%;
       height: 100%;
       object-fit: cover;
+      display: none;
+      &.visible {
+        display: block;
+      }
     }
   }
 }
