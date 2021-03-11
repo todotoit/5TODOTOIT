82a1fe6e217303530f3db41a19f7f75bbd6a523d
Kerem Türkyilmaz <kerem.turkyilmaz@outlook.it>
2021-03-11 18:28:34 +0100

    handling safari loadedmetadata event

--- components/Preloader.vue
+++ components/Preloader.vue
@@ -21,60 +21,33 @@ export default {
   },
   computed: {
     videoSrc() {
-      if (this.videos && this.videos.length > this.current)
-        return this.videos[this.current]
+      if (this.videos && this.videos.length > this.current) return this.videos[this.current]
       return ''
     }
   },
   mounted() {
-    this.boundCheckReady = this.checkVideoIsReady.bind(this)
-    this.interval = setInterval(this.boundCheckReady, 100)
     if (this.canPlay(this.$refs.video)) this.next()
+
+    const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent)
+    console.log('isSafari', isSafari)
+
+    const event = isSafari ? 'loadedmetadata' : 'canplay'
+
+    this.$refs.video.addEventListener(event, () => {
+      console.log(this.current + ' ' + event + ' - ready state:  this.$refs.video.readyState')
+      this.next()
+      this.$nextTick(() => {
+        this.$refs.video.load()
+      })
+    })
   },
   methods: {
     canPlay(video) {
-      return this.getLoadedPercent(video) >= 0.2
-    },
-    getLoadedPercent(video) {
-      let percent = 0
-      // FF4+, Chrome
-      if (
-        video &&
-        video.buffered &&
-        video.buffered.length > 0 &&
-        video.buffered.end &&
-        video.duration
-      ) {
-        percent = video.buffered.end(0) / video.duration
-      }
-      // e.g., FF3.6 and Safari 5
-      else if (
-        video &&
-        video.bytesTotal !== undefined &&
-        video.bytesTotal > 0 &&
-        video.bufferedBytes !== undefined
-      ) {
-        percent = video.bufferedBytes / video.bytesTotal
-      }
-      return percent
-    },
-    checkVideoIsReady() {
-      const playable = this.getLoadedPercent(this.$refs.video)
-      console.log(playable.toFixed(5))
-      if (this.canPlay(this.$refs.video)) {
-        console.log(
-          `${this.current} canplay - ready state: ${
-            this.$refs.video.readyState
-          } ready percent: ${playable.toFixed(1)}`
-        )
-        this.next()
-      }
+      return video.readyState === 4
     },
     next() {
-      console.log('next')
       this.current++
       if (this.current >= this.videos.length) {
-        clearInterval(this.interval)
         setTimeout(() => {
           this.done = true
         }, 0.2)
