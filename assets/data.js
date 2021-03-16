1d60404d635892a17173d519763f9b0aaea76032
Emilio Bondioli <e.bondioli@todo.to.it>
2021-03-16 10:56:40 +0100

    disable absolute urls

--- assets/data.js
+++ assets/data.js
@@ -1,3 +1,6 @@
+const useAbsoluteUrls = false
+const prefix = useAbsoluteUrls ? 'https://todotoit5.netlify.app/' : ''
+
 export const substatements = [
   {
     id: 1,
@@ -5,12 +8,12 @@ export const substatements = [
     copy: 'We create interactive exhibits, turning new ideas into truly unique experiences that people enjoy and remember.',
     file: {
       poster: {
-        horizontal: 'https://todotoit5.netlify.app/videos/desktop/01_Poster.jpg',
-        vertical: 'https://todotoit5.netlify.app/videos/mobile/01_Poster.jpg'
+        horizontal: prefix + '/videos/desktop/01_Poster.jpg',
+        vertical: prefix + '/videos/mobile/01_Poster.jpg'
       },
       videos: {
-        horizontal: 'https://todotoit5.netlify.app/videos/desktop/01_InteractiveExhibit.mp4',
-        vertical: 'https://todotoit5.netlify.app/videos/mobile/01_InteractiveExhibit.mp4'
+        horizontal: prefix + '/videos/desktop/01_InteractiveExhibit.mp4',
+        vertical: prefix + '/videos/mobile/01_InteractiveExhibit.mp4'
       }
     }
   },
@@ -20,12 +23,12 @@ export const substatements = [
     copy: 'We design and deliver kinetic installations, with technical mastery and beauty. Always finding new ways to stand out.',
     file: {
       poster: {
-        horizontal: 'https://todotoit5.netlify.app/videos/desktop/02_Poster.jpg',
-        vertical: 'https://todotoit5.netlify.app/videos/mobile/02_Poster.jpg'
+        horizontal: prefix + '/videos/desktop/02_Poster.jpg',
+        vertical: prefix + '/videos/mobile/02_Poster.jpg'
       },
       videos: {
-        horizontal: 'https://todotoit5.netlify.app/videos/desktop/02_KineticInstallations.mp4',
-        vertical: 'https://todotoit5.netlify.app/videos/mobile/02_KineticInstallations.mp4'
+        horizontal: prefix + '/videos/desktop/02_KineticInstallations.mp4',
+        vertical: prefix + '/videos/mobile/02_KineticInstallations.mp4'
       }
     }
   },
@@ -35,12 +38,12 @@ export const substatements = [
     copy: 'We unleash data\'s untapped potential by sensing the environment and turning numbers into engaging content and stories for your audience.',
     file: {
       poster: {
-        horizontal: 'https://todotoit5.netlify.app/videos/desktop/03_Poster.jpg',
-        vertical: 'https://todotoit5.netlify.app/videos/mobile/03_Poster.jpg'
+        horizontal: prefix + '/videos/desktop/03_Poster.jpg',
+        vertical: prefix + '/videos/mobile/03_Poster.jpg'
       },
       videos: {
-        horizontal: 'https://todotoit5.netlify.app/videos/desktop/03_Dataviz.mp4',
-        vertical: 'https://todotoit5.netlify.app/videos/mobile/03_Dataviz.mp4'
+        horizontal: prefix + '/videos/desktop/03_Dataviz.mp4',
+        vertical: prefix + '/videos/mobile/03_Dataviz.mp4'
       }
     }
   },
@@ -50,12 +53,12 @@ export const substatements = [
     copy: 'We craft strong concepts and tailor digital communication, to shape and improve the relationship between people and brands.',
     file: {
       poster: {
-        horizontal: 'https://todotoit5.netlify.app/videos/desktop/04_Poster.jpg',
-        vertical: 'https://todotoit5.netlify.app/videos/mobile/04_Poster.jpg'
+        horizontal: prefix + '/videos/desktop/04_Poster.jpg',
+        vertical: prefix + '/videos/mobile/04_Poster.jpg'
       },
       videos: {
-        horizontal: 'https://todotoit5.netlify.app/videos/desktop/04_DigitalProducts.mp4',
-        vertical: 'https://todotoit5.netlify.app/videos/mobile/04_DigitalProducts.mp4'
+        horizontal: prefix + '/videos/desktop/04_DigitalProducts.mp4',
+        vertical: prefix + '/videos/mobile/04_DigitalProducts.mp4'
       }
     }
   },
@@ -65,12 +68,12 @@ export const substatements = [
     copy: 'We leverage a wide spectrum of design tools to explain what matters to you, making even the most complex idea engaging for everyone.',
     file: {
       poster: {
-        horizontal: 'https://todotoit5.netlify.app/videos/desktop/05_Poster.jpg',
-        vertical: 'https://todotoit5.netlify.app/videos/mobile/05_Poster.jpg'
+        horizontal: prefix + '/videos/desktop/05_Poster.jpg',
+        vertical: prefix + '/videos/mobile/05_Poster.jpg'
       },
       videos: {
-        horizontal: 'https://todotoit5.netlify.app/videos/desktop/05_Museum.mp4',
-        vertical: 'https://todotoit5.netlify.app/videos/mobile/05_Museum.mp4'
+        horizontal: prefix + '/videos/desktop/05_Museum.mp4',
+        vertical: prefix + '/videos/mobile/05_Museum.mp4'
       }
     }
   }
@@ -81,72 +84,72 @@ export const team = [
     id: 1,
     name: 'Andrea Clemente',
     jobs: 'Co-Founder & Managing director',
-    file: 'https://todotoit5.netlify.app/team/AndreaClemente.mp4'
+    file: prefix + '/team/AndreaClemente.mp4'
   },
   {
     id: 2,
     name: 'Fabio Franchino',
     jobs: 'Co-Founder & CTO',
-    file: 'https://todotoit5.netlify.app/team/FabioFranchino.mp4'
+    file: prefix + '/team/FabioFranchino.mp4'
   },
   {
     id: 3,
     name: 'Julie Faure',
     jobs: 'Project Manager',
-    file: 'https://todotoit5.netlify.app/team/JulieFaure.mp4'
+    file: prefix + '/team/JulieFaure.mp4'
   },
   {
     id: 4,
     name: 'Luca Zanconi',
     jobs: 'Technical Artist & Software Developer',
-    file: 'https://todotoit5.netlify.app/team/LucaZanconi.mp4'
+    file: prefix + '/team/LucaZanconi.mp4'
   },
   {
     id: 5,
     name: 'Emilio Bondioli',
     jobs: 'Interaction designer & Developer',
-    file: 'https://todotoit5.netlify.app/team/EmilioBondioli.mp4'
+    file: prefix + '/team/EmilioBondioli.mp4'
   },
   {
     id: 6,
     name: 'Kerem Türkyilmaz',
     jobs: 'Graphic Designer & Junior Developer',
-    file: 'https://todotoit5.netlify.app/team/KeremTurkyilmaz.mp4'
+    file: prefix + '/team/KeremTurkyilmaz.mp4'
   },
   {
     id: 7,
     name: 'Marta Monge',
     jobs: 'Art Director & Designer',
-    file: 'https://todotoit5.netlify.app/team/MartaMonge.mp4'
+    file: prefix + '/team/MartaMonge.mp4'
   },
   {
     id: 8,
     name: 'Federica Zucchini',
     jobs: 'Visual Designer',
-    file: 'https://todotoit5.netlify.app/team/FedericaZucchini.mp4'
+    file: prefix + '/team/FedericaZucchini.mp4'
   },
   {
     id: 9,
     name: 'Matteo Palù',
     jobs: 'Information Designer',
-    file: 'https://todotoit5.netlify.app/team/MatteoPalu.mp4'
+    file: prefix + '/team/MatteoPalu.mp4'
   },
   {
     id: 10,
     name: 'Alice Mela',
     jobs: 'Design Director & Interaction Designer',
-    file: 'https://todotoit5.netlify.app/team/AliceMela.mp4'
+    file: prefix + '/team/AliceMela.mp4'
   },
   {
     id: 11,
     name: 'Virginia Toffetti',
     jobs: 'Product Designer',
-    file: 'https://todotoit5.netlify.app/team/VirginiaToffetti.mp4'
+    file: prefix + '/team/VirginiaToffetti.mp4'
   },
   {
     id: 12,
     name: 'Alessandra Martina',
     jobs: 'Administrative Assistant',
-    file: 'https://todotoit5.netlify.app/team/AlessandraMartina.mp4'
+    file: prefix + '/team/AlessandraMartina.mp4'
   }
 ]
