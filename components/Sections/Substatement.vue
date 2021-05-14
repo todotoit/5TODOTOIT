9113a544c0d164e3a9c0fd3e0aeddbad1ccf9b8a
Kerem Türkyilmaz <kerem.turkyilmaz@outlook.it>
2021-05-14 12:40:14 +0200

    feat: linear gradient on top of the video

--- components/Sections/Substatement.vue
+++ components/Sections/Substatement.vue
@@ -32,6 +32,7 @@
           ></video>
         </transition>
       </div>
+      <div class="gradient" />
     </div>
   </section>
 </template>
@@ -79,7 +80,6 @@ export default {
       position: absolute;
       top: 0;
       video {
-        filter: brightness(43%);
         width: 100%;
         height: 100%;
         object-fit: cover;
@@ -87,6 +87,15 @@ export default {
       &.visible {
         display: block;
       }
+    }
+    .gradient {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      z-index: 10;
+      background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0) 60%);
     }
   }
 }
